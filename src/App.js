8b7107b920a99526b73ab2ec2f98a5edf6c0fb6f
milena <m.sivit@itecriocuarto.org.ar>
2024-10-22 17:13:36 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css'; // Importar el CSS reset de Ant Design
+
+const App = () => (
+  <div style={{ padding: '50px' }}>
+    <h1>aguante Ant Design vieja</h1>
+    <Button type="primary">Botón de Ant Design</Button>
+  </div>
+);
+
+export default App;
